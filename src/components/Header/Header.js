5e670a879091276d6cdf59e0f55f6d2ea4540551
Ridoy-abdullah <abdullahalm705@gmail.com>
2022-10-07 19:14:09 +0600

    add

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,6 +7,7 @@ const Header = () => {
         <nav className='header'>
             <img src={logo} alt="" />
             <div>
+                 <a href="/contact">Contact</a>
                 <a href="/shop">Shop</a>
                 <a href="/orders">Orders</a>
                 <a href="/inventory">Inventory</a>
